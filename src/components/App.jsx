d55e24fc9346ed81bd17800adc5f967f9acf2c08
Mahany <mhaniamr3@gmail.com>
2025-01-28 12:18:21 +0000

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
-      <Footer />
-    </div>
-  );
+  const[notes,setNotes]=useState([]);
+
+  function AddNote(newNote){
+    setNotes((prev)=>{
+      return[...prev,newNote];
+    });
+  }
+ function deleteNote(id){
+ setNotes((prev)=>{
+  return prev.filter((note,index)=>{
+    return index !== id;
+  });
+ });
+ }  
+
+ return (
+  <div>
+    <Header />
+    <CreateArea onAdd={AddNote} />
+    {notes.map((note, index) => {
+      return (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      );
+    })}
+    <Footer />
+  </div>
+);
 }
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note,setNote]=useState({
+    title: "",
+    content:"",
+  });
+
+  function handleChange(e){
+    const {name , value} = e.target;
+
+    setNote((prev)=>{
+      return {
+        ...prev,[name]:value
+      };
+    });
+  }
+  function submit(e){
+  props.onAdd(note);
+  setNote({
+    title:"",
+    content:""
+  });
+  e.preventDefault();
+
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" value={note.title} onChange={handleChange} />
+        <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} onChange={handleChange} />
+        <button onClick={submit} disabled={!note.title|| !note.content}>Add</button>
       </form>
     </div>
   );
